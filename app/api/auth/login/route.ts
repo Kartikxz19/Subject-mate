5c5716d619b49c7e64a236a523edab4d393a5d4a
Kartik Srivastava <kartiksri1911@gmail.com>
2024-05-25 00:52:19 +0530

    debugging

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -9,24 +9,25 @@ export async function POST(request: Request) {
   try {
     const body = await request.json();
     const { identifier, password } = body; // identifier can be enrollment, email, or phone
-
+    console.log("Hello1");
     // Check if user exists
     const userQuery = await sql`
       SELECT * FROM auth 
       WHERE email = ${identifier} OR phone = ${identifier}
     `;
-
+    console.log("Hello1");
     if (userQuery.rows.length === 0) {
       return ErrorResponse('Invalid credentials', 400);
     }
-
+    console.log("Hello2");
     const user = userQuery.rows[0];
-
+    console.log("Hello3");
     // Check if password matches
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
       return ErrorResponse('Invalid credentials', 400);
     }
+    console.log("Hello4");
 
     // Generate JWT token
     const token = jwt.sign(
@@ -34,7 +35,7 @@ export async function POST(request: Request) {
       JWT_SECRET,
       { expiresIn: '30d' } // Token validity period
     );
-
+    console.log("Hello5");
     // Return success response with the token
     return Response.json({ token });
 
